17ddc69d651f28c433fce248731d64fedd3ccbbb
Aym :) <github.perfected536@passinbox.com>
2026-06-21 12:07:00 +0200

    fix: until we use an opaque pointer, we need to null the data members and check data in the free method to prevent crashes

--- libs/http_server/src/bzf_http_server_destroy.c
+++ libs/http_server/src/bzf_http_server_destroy.c
@@ -11,5 +11,8 @@ void bzf_http_server_destroy(struct bzf_http_server* http_server) {
         bzf_close(http_server->file_descriptor);
     }
 
-    bzf_hashmap_free(http_server->handlers.configured_routes, &bzf_http_free_base);
+    if (http_server->handlers.configured_routes != NULL)
+    {
+        bzf_hashmap_free(http_server->handlers.configured_routes, &bzf_http_free_base);
+    }
 }
